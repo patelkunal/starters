8aaedb168e81aff65c2a7c0a5ef1bd309a08fe76
Kunal Patel <kunal_patel@symantec.com>
2016-01-27 11:21:20 +0530

    added auth filter class placeholder

---
+++ spring-secured-rest/src/main/java/org/coderearth/security/filter/CustomAPIAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package org.coderearth.security.filter;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+public class CustomAPIAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+	protected CustomAPIAuthenticationFilter(String defaultFilterProcessesUrl) {
+		super(defaultFilterProcessesUrl);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
